3469d59a55d546a9f2af3cf711e7e61957ac0f12
Josh Worden <josh@parameter1.com>
2022-12-08 15:16:55 -0600

    Add Auth0 tenant config

--- sites/drbicuspid.com/config/auth0.js
+++ sites/drbicuspid.com/config/auth0.js
@@ -3,4 +3,5 @@ module.exports = {
   clientID: process.env.AUTH0_CLIENTID || '8aHKTbVEAMFdtF3S2sA67zoprlfT2TVw',
   clientSecret: process.env.AUTH0_SECRET,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASEURL || 'https://dev-scienceandmedicinegroup.us.auth0.com',
+  tenant: process.env.AUTH0_TENANT || 'dev-scienceandmedicinegroup',
 };
