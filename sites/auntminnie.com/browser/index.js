7aa5c0a1bddd2f8564e60f2c873f690beb10f1f5
Shinsina <shinsina@att.net>
2023-08-10 11:27:39 -0500

    Passthrough custom profile form

--- sites/auntminnie.com/browser/index.js
+++ sites/auntminnie.com/browser/index.js
@@ -1,6 +1,7 @@
 import Browser from '@parameter1/base-cms-marko-web/browser';
 import Shared from '@science-medicine-group/package-global/browser';
+import CustomProfileComponent from './profile-form.vue';
 
-Shared(Browser);
+Shared(Browser, { CustomProfileComponent });
 
 export default Browser;
