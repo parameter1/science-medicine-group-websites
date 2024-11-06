db929ea3e4a9c7d439a5c7551d3806a21c751d7b
Jessica Wade <jessica.wade@parameter1.com>
2024-11-06 11:48:21 -0600

    Remove comment for ‘resources’ and fix misspelling of RSNA

--- sites/auntminnie.com/config/gam.js
+++ sites/auntminnie.com/config/gam.js
@@ -71,10 +71,10 @@ const aliases = [
   { alias: 'resources/conference/ismrm/2024', prefix: 'ismrm-2024' },
   { alias: 'resources/conference/snmmi/2024', prefix: 'snmmi-2024' },
   { alias: 'resources/conference/cmimi/2024', prefix: 'cmimi-2024' },
-  { alias: 'resources/conference/rnsa/2024', prefix: 'rsna-2024' },
+  { alias: 'resources/conference/rsna/2024', prefix: 'rsna-2024' },
   // Sections
   { alias: 'demo' },
-  // { alias: 'resources' },
+  { alias: 'resources' },
 ];
 
 aliases.forEach(({ alias, prefix = alias }) => config.setAliasAdUnits(alias, [
